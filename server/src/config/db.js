a45b1717e44ec8df100b9076397724297b9d13cf
Romina Curbelo <curbelonobleromina@gmail.com>
2026-04-15 22:53:28 -0300

    Agrega coneccion a base de datos y modelos de entidades

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,40 +1,16 @@
 import mongoose from "mongoose";
 
-// getting-started.js
-const mongoose = require('mongoose');
-
-main().catch(err => console.log(err));
-
-export async function connectDB() {
-  const connection = await mongoose.connect('mongodb://'); // replace with your MongoDB connection string
-  if (connection) {
-    console.log("Connected to MongoDB");
+const conectarBD = async () => {
+  const nombreDB = process.env.MONGO_DB_NAME
+  const user = process.env.MONGO_DB_USER
+  const password = process.env.MONGO_DB_PASS
+  try{
+    await mongoose.connect(`mongodb+srv://${user}}:${password}@${nombreDB}.e98wusg.mongodb.net/?appName=${nombreDB}`)
+    console.log("la base de datos se conectó con éxito")
+  } catch (e){
+    console.log("Error al conectar la base de datos")
+    process.exit(1);
   }
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-const UserSchema = new mongoose.Schema({
-  id: Number,
-   name: String,
-   email: String,
-   password: String
-});
-
-const BookSchema = new mongoose.Schema({
-  id: Number,
-   title: String,
-   author: String,
-    publishedDate: Date
-    });
-
-const ReviewSchema = new mongoose.Schema({
-    id: Number,
-     bookId: Number,
-     userId: Number,
-     rating: Number,
-     comment: String
-});
-
-export const UserModel = mongoose.model('User', UserSchema);
-export const BookModel = mongoose.model('Book', BookSchema);
-export const ReviewModel = mongoose.model('Review', ReviewSchema);
+export { conectarBD }
