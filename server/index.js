abf9ccb5fdf775816af1b793a9a0700e88027a14
Romina Curbelo <curbelonobleromina@gmail.com>
2026-04-26 18:14:49 -0300

    Adds logger

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import { adminAuthorizationMiddleware } from "./src/middleware/admin.middleware.
 import { adminRouterV1 } from "./src/routes/admin.router.v1.js";
 import { reviewRouterV1 } from "./src/routes/review.router.v1.js";
 import { userRouterV1 } from "./src/routes/user.router.v1.js";
+import { logMiddleware } from "./src/middleware/logger.middleware.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,11 +16,13 @@ const host = "localhost";
 
 app.use(express.json());
 
+app.use(logMiddleware);
+
 //Rutas públicas
 app.use("/v1", authRouter)
   
 //Rutas privadas
-app.use(authMiddleware)
+app.use(authMiddleware);
 app.use("/v1/books", bookRouterV1);
 app.use("/v1/review", reviewRouterV1);
 app.use("/v1/user", userRouterV1);
