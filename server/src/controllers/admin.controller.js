83276b49f9edd6a71174f6ec285cc6eddea94e56
Romina Curbelo <curbelonobleromina@gmail.com>
2026-04-29 18:19:07 -0300

    Adds getAllBooks and getAllReviews endpoints

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,6 +1,7 @@
 import { obtenerInfoAdmin } from "../services/admin.service.v1.js";
+import { obtenerLibrosService, obtenerReviewsService } from "../services/admin.service.v1.js";
 
-const obtenerAdmin = async (req, res) => {
+export const obtenerAdmin = async (req, res) => {
     try {
         const admin = await obtenerInfoAdmin();
         res.status(200).json(admin);
@@ -9,4 +10,21 @@ const obtenerAdmin = async (req, res) => {
     }
 };
 
-export { obtenerAdmin };
+export const obtenerLibros = async (req,res) => {
+    try{
+        const librosUsuarios = await obtenerLibrosService();
+        res.status(200).json(librosUsuarios)
+    }catch(e){
+        res.status(e.code).json({ error: e.error })
+    }
+
+};
+
+export const obtenerReviews = async (req, res) => {
+    try{
+        const reviewsUsuarios = await obtenerReviewsService();
+        res.status(200).json(reviewsUsuarios)
+    }catch(e){
+        res.status(e.code).json({ error: e.error })
+    }
+};
--- server/src/services/admin.service.v1.js
+++ server/src/services/admin.service.v1.js
@@ -1,7 +1,18 @@
-const obtenerInfoAdmin = async () => {
+import { Libro } from "../models/book.model.js"
+import { Review } from "../models/review.model.js"
+
+export const obtenerInfoAdmin = async () => {
     return {
         message: "Bienvenido al panel de administracion"
     };
 };
 
-export { obtenerInfoAdmin };
+export const obtenerLibrosService = async () => {
+    const librosTodos = await Libro.find();
+    return librosTodos;
+};
+
+export const obtenerReviewsService = async () => {
+    const reviewsTodas = await Review.find();
+    return reviewsTodas; 
+};
