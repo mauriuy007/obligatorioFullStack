7e3b1c81a08f4b95687ed130b5cd6fd3e7fd9f25
Romina Curbelo <curbelonobleromina@gmail.com>
2026-06-07 13:29:56 -0300

    Add allow() method to autor, genero, and descripcion fields in book schema

--- server/src/validators/book.validator.js
+++ server/src/validators/book.validator.js
@@ -4,13 +4,13 @@ const bookSchema = Joi.object({
     titulo: Joi.string().trim().min(1).required().messages({
         "string.empty": "El titulo no puede estar vacío"
     }),
-    autor: Joi.string().trim().min(2).max(70).messages({
+    autor: Joi.string().trim().min(2).max(70).allow('').messages({
         "string.max": "Este campo puede tener máximo 70 caracteres"
     }),
-    genero: Joi.string().trim().min(1).max(30).messages({
+    genero: Joi.string().trim().min(1).max(30).allow('').messages({
         "string.max": "Este campo puede tener máximo 30 caracteres"
     }),
-    descripcion: Joi.string().trim().min(30).max(1000).messages({
+    descripcion: Joi.string().trim().min(30).max(1000).allow('').messages({
         "string.max": "Este campo puede tener máximo 1000 caracteres",
         "string.min": "La descripción debe tener al menos 30 caracteres"
 
